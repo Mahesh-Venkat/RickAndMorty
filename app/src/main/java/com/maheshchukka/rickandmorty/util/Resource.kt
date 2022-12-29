eb0a4bb1fafe1f5fe3c7169d85e6902fc0c7da92
Mahesh Chukka <mahesh.chukka@carvana.com>
2022-12-29 10:10:13 -0500

    A base resource folder is which gets used in the API layer data wrapping

---
+++ app/src/main/java/com/maheshchukka/rickandmorty/util/Resource.kt
@@ -0,0 +1,10 @@
+package com.maheshchukka.rickandmorty.util
+
+sealed class Resource<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+    class Success<T>(data: T?) : Resource<T>(data)
+    class Error<T>(message: String?, data: T?) : Resource<T>(message = message, data = data)
+    class Loading<T>(val isLoading: Boolean = true) : Resource<T>(data = null)
+}
