3d7142b3845be5502a8aff4e97890eb62c41f6ff
Mahesh Chukka <mahesh.chukka@carvana.com>
2022-12-29 10:09:33 -0500

    Added a basic network layer to hit the characters end point

---
+++ app/src/main/java/com/maheshchukka/rickandmorty/data/remote/RickyAndMortyApi.kt
@@ -0,0 +1,14 @@
+package com.maheshchukka.rickandmorty.data.remote
+
+import com.maheshchukka.rickandmorty.data.remote.dto.CharacterDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RickyAndMortyApi {
+    @GET("api/character")
+    suspend fun getCharacters(@Query("page") page: Int): CharacterDto
+
+    companion object {
+        const val BASE_URL = "https://rickandmortyapi.com/"
+    }
+}
---
+++ app/src/main/java/com/maheshchukka/rickandmorty/data/remote/dto/CharacterDto.kt
@@ -0,0 +1,33 @@
+package com.maheshchukka.rickandmorty.data.remote.dto
+
+import com.squareup.moshi.Json
+
+data class CharacterDto(
+    @field:Json(name = "info") val info: CharacterPagingInfo,
+    @field:Json(name = "results") val results: List<CharacterResult>
+)
+
+data class CharacterPagingInfo(
+    @field:Json(name = "count") val count: Int?,
+    @field:Json(name = "pages") val pages: Int?,
+    @field:Json(name = "next") val next: String?,
+    @field:Json(name = "prev") val prev: Int?
+)
+
+data class CharacterResult(
+    @field:Json(name = "id") val id: Long,
+    @field:Json(name = "name") val name: String?,
+    @field:Json(name = "status") val status: String?,
+    @field:Json(name = "species") val species: String?,
+    @field:Json(name = "type") val type: String,
+    @field:Json(name = "gender") val gender: String,
+    @field:Json(name = "origin") val origin: OriginOrLocation?,
+    @field:Json(name = "location") val location: OriginOrLocation?,
+    @field:Json(name = "image") val imageUrl: String?,
+    @field:Json(name = "episode") val episodes: List<String>?
+)
+
+data class OriginOrLocation(
+    @field:Json(name = "name") val name: String?,
+    @field:Json(name = "url") val url: String?
+)
